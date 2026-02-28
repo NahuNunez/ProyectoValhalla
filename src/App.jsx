873fe3a00bc87815409dd186efd52d8da289ac28
Nahuel Nuñez <nunezn53@gmail.com>
2026-02-28 09:12:53 -0300

    arregloo error vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 //import { useState } from 'react'
 //import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/pages/Home/Home'
 
 
